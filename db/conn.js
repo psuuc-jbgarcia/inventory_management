0846ec0aed7772156c334afee9862a6bea2994f5
jerico <gjeric54321@gmail.com>
2025-02-24 22:24:02 +0800

    "try"

--- db/conn.js
+++ db/conn.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
 const mysql = require('mysql2')
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    user:process.env.DB_USER
-})
-
-db.connect((err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log("Connected!")
-    }
-})
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password:process.env.DB_PASSWORD,
+//     user:process.env.DB_USER
+// })
+const urlDB=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`
+const db=mysql.createConnection(urlDB);
+// db.connect((err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Connected!")
+//     }
+// })
 
 module.exports = db;
