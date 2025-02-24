b6c28174f584670e3006bb7f07d98b0b098bdb6a
jerico <gjeric54321@gmail.com>
2025-02-24 22:08:12 +0800

    Add .env to .gitignore

--- db/conn.js
+++ db/conn.js
@@ -1,14 +1,19 @@
-const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    user:process.env.DB_USER
+})
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the database!');
-});
+db.connect((err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Connected!")
+    }
+})
 
+module.exports = db;
