73670bb7f8946a3ad4232e9ac228661db44c4f49
jerico <gjeric54321@gmail.com>
2025-02-25 18:57:11 +0800

    "SS"

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/products/delete/:id', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
